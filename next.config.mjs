3d58d9b880fdfb290b8b2dc2fc744c683718d40f
Ygor Carvalho Pc pessoal <cygor479@gmail.com>
2025-12-28 13:55:55 -0300

    Troca next.config.ts por mjs para compatibilidade

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Se tiver alguma configuração específica, coloque aqui.
+  // Por enquanto, deixamos vazio para funcionar o básico.
+};
+
+export default nextConfig;
